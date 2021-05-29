9bb6537d01b2bf85d08137cce3bfce7ffe7bca2e
ArchibaldHof <artik.kh@gmail.com>
2021-05-29 13:44:34 +0300

    update commit

--- client_side/settings.py
+++ client_side/settings.py
@@ -80,10 +80,7 @@ WSGI_APPLICATION = "client_side.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(conn_max_age=600, default=config("DB_ID"))
 }
 
 # Password validation
